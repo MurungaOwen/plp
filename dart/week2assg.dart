088ff6668018675ba0283ea5dec34d7d632355ae
MurungaOwen <owenhood80@gmail.com>
2024-03-08 17:08:56 +0300

    finish up the recipe

---
+++ dart/week2assg.dart
@@ -0,0 +1,25 @@
+int addTwo(int num1, int num2) {
+  return num1 + num2;
+}
+//task 1 above
+
+int subtractTwo(int num1, int num2) {
+  return num1 - num2;
+}
+//task 2 above
+
+int multiplyTwo(int num1, int num2) {
+  return num1 * num2;
+}
+//task 3 above
+
+double divideTwo(int num1, int num2) {
+  return num1 / num2;
+}
+//task 4 above
+
+
+void main() {
+  print(addTwo(2, 8));
+  print(divideTwo(6, 7));
+}
